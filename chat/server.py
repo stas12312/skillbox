7be61604f64553b0b0e527cff48a522b6db3b41e
Stanislav Rush <911rush@gmail.com>
2019-06-15 19:27:57 +0700

    + Добавлена проверка на занятость логина + Добавлена история сообщений * Сервер не отправляет сообщение отправителю

--- chat/server.py
+++ chat/server.py
@@ -23,7 +23,7 @@ class Client(Protocol):
 
         print(f"Client connected: {self.ip}")
 
-        self.transport.write("Welcome to the chat v0.1\n".encode())
+        self.transport.write("Welcome to the chat v0.2\n".encode())
 
     def dataReceived(self, data: bytes):
         """
@@ -34,17 +34,24 @@ class Client(Protocol):
 
         if self.login is not None:
             server_message = f"{self.login}: {message}"
-            self.factory.notify_all_users(server_message)
-
+            self.factory.notify_all_users(server_message, self)
             print(server_message)
         else:
             if message.startswith("login:"):
-                self.login = message.replace("login:", "")
+                login = message.replace("login:", "")
 
-                notification = f"New user connected: {self.login}"
+                # Проверяем занятость логина
+                if self.factory.check_login(login):
+                    self.transport.write(f"Error: Login {login} is exist".encode())
+                    reactor.callLater(2, self.transport.loseConnection)
+                    print(f'Error: Login {login} is exist')
+                else:
+                    self.login = login
+                    self.factory.send_history(self)  # Отправляем клиенту историю сообщений
+                    notification = f"New user connected: {self.login}"
 
-                self.factory.notify_all_users(notification)
-                print(notification)
+                    self.factory.notify_all_users(notification, self)
+                    print(notification)
             else:
                 print("Error: Invalid client login")
 
@@ -59,11 +66,13 @@ class Client(Protocol):
 
 class Chat(Factory):
     clients: list
+    history: str  # Строка для хранения отправленных сообщений
 
     def __init__(self):
         """
         Инициализация сервера
         """
+        self.history = ''
         self.clients = []
         print("*" * 10, "\nStart server \nCompleted [OK]")
 
@@ -82,14 +91,41 @@ class Chat(Factory):
         """
         return Client(self)
 
-    def notify_all_users(self, data: str):
+    def notify_all_users(self, data: str, sender: Client = None):
         """
         Отправка сообщений всем текущим пользователям
         :param data:
+        :param sender:
         :return:
         """
-        for user in self.clients:
+        self.history += f'{data}\n'  # Добавляем сообщение в историю
+
+        # Копируем список и удаляем из него отправителя
+        clients = self.clients[:]
+        clients.remove(sender)
+
+        # Отправка сообщения всем, кроме отправителя
+        for user in clients:
             user.transport.write(f"{data}\n".encode())
+
+    def check_login(self, login: str):
+        """
+        Проверка занятости логина
+        :param login:
+        :return:
+        """
+        for client in self.clients:
+            if client.login == login:
+                return True
+        return False
+
+    def send_history(self, client: Client):
+        """
+        Отправляет историю сообщений клиенту
+        :param client:
+        :return:
+        """
+        client.transport.write(self.history.encode())
 
 
 if __name__ == '__main__':
